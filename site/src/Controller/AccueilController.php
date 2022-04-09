2d969313244b4349042e6ee5305d7b7d5406f844
Lenspawk <simonkortsmit@gmail.com>
2022-04-09 16:00:54 +0200

    Accueil
    Controlleur de base pour l'index et un template

---
+++ site/src/Controller/AccueilController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/Accueil', name : 'accueil_')]
+
+class AccueilController extends AbstractController
+{
+    #[Route('', name: 'index')]
+    public function indexAction(): Response
+    {
+        return $this->render('accueil/index.html.twig', [
+            'user' => $this->getUser(),
+            'welcome' => 'Nous te souhaitons la bienvenue sur le site',
+            'nameSite' => '[NOM_DU_SITE]',
+        ]);
+    }
+}
